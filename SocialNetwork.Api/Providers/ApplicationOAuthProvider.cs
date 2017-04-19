62040e7cdd8e2477120bf1e2ba100ef77693e33d
Munir Wanis <fallen.angel.pp@gmail.com>
2017-04-19 11:48:38 -0300

    :construction: Adds OAuthProvider

--- SocialNetwork.Api/Providers/ApplicationOAuthProvider.cs
+++ SocialNetwork.Api/Providers/ApplicationOAuthProvider.cs
@@ -1,11 +1,42 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OAuth;
+using SocialNetwork.Api.App_Start;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Threading.Tasks;
+using SocialNetwork.Api.Models;
 
 namespace SocialNetwork.Api.Providers
 {
-    public class ApplicationOAuthProvider
+    public class ApplicationOAuthProvider : OAuthAuthorizationServerProvider
     {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var userManager = context.OwinContext.GetUserManager<ApplicationUserManager>();
+            var user = await userManager.FindAsync(context.UserName, context.Password);
+
+            if (user != null)
+            {
+                var oAuthIdentity = await user.GenerateUserIdentityAsync(userManager, OAuthDefaults.AuthenticationType);
+                AuthenticationProperties properties = CreateProperties(user);
+                var ticket = new AuthenticationTicket(oAuthIdentity, properties);
+            }
+
+            context.SetError("invalid_grant", "The UserName or Password is incorrect");
+        }
+
+        private AuthenticationProperties CreateProperties(ApplicationUser user)
+        {
+            var data = new Dictionary<string, string>
+            {
+                {"UserName", user.UserName }
+            };
+            return new AuthenticationProperties(data);
+        }
     }
 }
