102d1a47e9b9b79f89dcb1124982042eb03ac6a3
Munir Wanis <fallen.angel.pp@gmail.com>
2017-04-19 12:03:01 -0300

    :construction: Creates partial startup class

---
+++ SocialNetwork.Api/App_Start/Startup.Auth.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using SocialNetwork.Api.App_Start;
+using SocialNetwork.Api.Models;
+using SocialNetwork.Api.Providers;
+using System;
+
+namespace SocialNetwork.Api
+{
+    public partial class Startup
+    {
+        public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
+        private void ConfigureAuth(IAppBuilder app)
+        {
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            OAuthOptions = new OAuthAuthorizationServerOptions
+            {
+                Provider = new ApplicationOAuthProvider(),
+                TokenEndpointPath = new PathString("/Token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1)
+            };
+            app.UseOAuthBearerTokens(OAuthOptions);
+            throw new NotImplementedException();
+        }
+    }
+}
--- SocialNetwork.Api/Startup.cs
+++ SocialNetwork.Api/Startup.cs
@@ -5,11 +5,12 @@ using System.Web.Http;
 [assembly: OwinStartup(typeof(SocialNetwork.Api.Startup))]
 namespace SocialNetwork.Api
 {
-    public class Startup
+    public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
             var httpConfiguration = new HttpConfiguration();
+            ConfigureAuth(app);
             WebApiConfig.Register(httpConfiguration);
             app.UseWebApi(httpConfiguration);
         }
